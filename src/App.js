a34a982cdcff8d68fc2d9485c0ed71cfc0950bfd
Prathamesh180 <85394583+Prathamesh180@users.noreply.github.com>
2023-04-23 11:52:03 +0530

    added about us section and dark mode feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 import Navbar from './components/Navbar'
-import Textbox from './components/Text'
+// import Textbox from './components/Text'
+import About from './components/about'
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
     <div className="container my-3">
-    <Textbox heading="Enter your text to edit"/>
+    <About/>
+    {/* <Textbox heading="Enter your text to edit"/> */}
     </div>
     </>
   )
